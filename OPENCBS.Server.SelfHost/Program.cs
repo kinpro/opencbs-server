d077cd955479d65b2a09322c8974ac0343ee19d7
Pavel Bastov <pbastov@gmail.com>
2015-03-05 20:46:23 +0600

    Make server stop on CTRL+C press

--- OPENCBS.Server.SelfHost/Program.cs
+++ OPENCBS.Server.SelfHost/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Threading;
 using Nancy.Hosting.Self;
 
 namespace OPENCBS.Server.SelfHost
 {
-	class MainClass
-	{
-		public static void Main()
-		{
-			var config = new HostConfiguration();
-			config.UrlReservations.CreateAutomatically = true;
-			using (var host = new NancyHost(config, new Uri("http://localhost:12345")))
-			{
-				host.Start();
-				Console.ReadLine();
-			}
-		}
-	}
+    class MainClass
+    {
+        public static void Main()
+        {
+            var exitEvent = new ManualResetEvent(false);
+
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                    args.Cancel = true;
+                    exitEvent.Set();
+            };
+            Console.WriteLine("OPENCBS server is running...");
+            Console.WriteLine("Press CTRL+C to stop");
+
+            var config = new HostConfiguration();
+            config.UrlReservations.CreateAutomatically = true;
+            using (var host = new NancyHost(config, new Uri("http://localhost:12345")))
+            {
+                host.Start();
+                exitEvent.WaitOne();
+            }
+        }
+    }
 }
